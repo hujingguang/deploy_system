473d4a842e27f9e7a627a75726eb51b39a274e9d
wuyang <hoo@wikiki.cn>
2016-01-29 10:27:02 +0800

    修复svn交互获取版本号bug

---
+++ required.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# 请安装python2.7 pip包管理工具
+
+
+pip install flask
+pip install sqlalchemy
+pip install Flask-SQLAlchemy
+pip install Flask-Login
+pip install flask-bootstrap
+pip install flask-pagination
+pip install flask-paginate
+pip install flask-wtf
